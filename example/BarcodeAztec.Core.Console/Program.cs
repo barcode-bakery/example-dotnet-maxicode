7921f5841ac572d52a6d5ecfabc3dd89184e2cdc
Jean-Sébastien Goupil <contact@jsgoupil.com>
2021-05-19 15:37:31 -0700

    Initial commit.

---
+++ example/BarcodeAztec.Core.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace BarcodeMaxiCode.Core.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // See each barcode file to see how you can save to a file or a MemoryStream.
+            await ExampleMaxiCode.CreateAsync("barcode_maxicode.png");
+        }
+    }
+}
